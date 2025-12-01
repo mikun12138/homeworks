ea642bfb0c3d07982a72754e584da624ece270f5
mikun_12138 <mikun.12138@gmail.com>
2025-12-02 02:45:51 +0800

    4

--- ex4/vite.config.ts
+++ ex4/vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "homeworks/ex4/"
+  base: "homeworks/ex4/",
+  build: {
+    outDir: "dist",
+  }
+
 })
